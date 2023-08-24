8d6f152c706f83f4091ff19ab36d1e25f00f1706
brian <braintm2@gmail.com>
2023-08-24 13:24:44 -0700

    created PortfolioItem component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,17 +1,56 @@
+import p1image from "../images/pp-ss.png";
+import p2image from "../images/ft-ss.png";
+import p3image from "../images/c4-ss.png";
+import PortfolioItem from "./PortfolioItem";
+
 export default function Portfolio() {
-    return (    
-        <section id="portfolio" className="section">
-          <h2>Portfolio</h2>
-          <div className="portfolio-item">
-            <img src="project1.jpg" alt="Project 1" />
-            <h3>Project 1</h3>
-            <p>Description of Project 1.</p>
-          </div>
-          <div className="portfolio-item">
-            <img src="project2.jpg" alt="Project 2" />
-            <h3>Project 2</h3>
-            <p>Description of Project 2.</p>
-          </div>
-        </section>
-    )
+  const projects = [
+    {
+      projectID: 1,
+      projectName: "Puppy Paradise",
+      projectImage: p1image,
+      projectURL: "https://fluffy-trifle-fba9bc.netlify.app/",
+      projectDescription: "Some text"
+    },
+    {
+      projectID: 2,
+      projectName: "Fitness Track.r",
+      projectImage: p2image,
+      projectURL: "https://relaxed-torrone-8e362c.netlify.app/",
+      projectDescription: "Some text"
+    },
+    {
+      projectID: 3,
+      projectName: "Connect 4",
+      projectImage: p3image,
+      projectURL: "https://subtle-khapse-befbf2.netlify.app/",
+      projectDescription: "Some text"
+    }
+  ]
+  return (    
+      <section id="portfolio" className="section">
+        <h2>Portfolio</h2>
+        {
+          projects.map(project => {
+            const {
+              projectID,
+              projectName,
+              projectImage,
+              projectURL,
+              projectDescription
+            } = project;
+
+            return(
+              <PortfolioItem
+                key={projectID}
+                projectName={projectName}
+                projectImage={projectImage}
+                projectURL={projectURL}
+                projectDescription={projectDescription}
+              />
+            )
+          })
+        }
+      </section>
+  )
 }
